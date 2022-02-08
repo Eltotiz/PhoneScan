4f008b206d51052263503a9d7d4763e802076244
Eltotiz <77215699+Eltotiz@users.noreply.github.com>
2022-02-08 19:09:32 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+clear
+echo Instalando paquetes.
+sleep 2
+pkg install python3
+pip install phone numbers
+clear
+sleep 1
+echo Paquetes instalados.
+echo " "
+echo Colocar python PhoneScan.py para ejecutarlo.
